83dbb9ea1d34fd49427973b645b6e48e46464cec
gricn <gricn666@gmail.com>
2021-06-19 00:20:40 +0800

    improve home page experience in large screen

--- components/Modal.js
+++ components/Modal.js
@@ -1,89 +1,69 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/self-closing-comp */
 import React from "react";
-import { withStyles } from "@material-ui/core/styles";
-import Button from "@material-ui/core/Button";
-import Dialog from "@material-ui/core/Dialog";
-import MuiDialogTitle from "@material-ui/core/DialogTitle";
-import MuiDialogContent from "@material-ui/core/DialogContent";
-import MuiDialogActions from "@material-ui/core/DialogActions";
-import IconButton from "@material-ui/core/IconButton";
-import CloseIcon from "@material-ui/icons/Close";
-import Typography from "@material-ui/core/Typography";
+import InfoIcon from '@material-ui/icons/Info';
 
-import modalStyles from "./Modal.module.css";
+export default function Modal(props) {
 
-const styles = (theme) => ({
-  root: {
-    margin: 0,
-    padding: theme.spacing(2),
-  },
-  closeButton: {
-    position: "absolute",
-    right: theme.spacing(1),
-    top: theme.spacing(1),
-    color: theme.palette.grey[500],
-  },
-});
-
-const DialogTitle = withStyles(styles)((props) => {
-  const {
-    children, classes, onClose, ...other
-  } = props;
-  return (
-    // eslint-disable-next-line react/jsx-props-no-spreading
-    <MuiDialogTitle disableTypography className={classes.root} {...other}>
-      <Typography variant="h6">{children}</Typography>
-      {onClose ? (
-        <IconButton aria-label="close" className={classes.closeButton} onClick={onClose}>
-          <CloseIcon />
-        </IconButton>
-      ) : null}
-    </MuiDialogTitle>
-  );
-});
-
-const DialogContent = withStyles((theme) => ({
-  root: {
-    padding: theme.spacing(2),
-  },
-}))(MuiDialogContent);
-
-const DialogActions = withStyles((theme) => ({
-  root: {
-    margin: 0,
-    padding: theme.spacing(1),
-  },
-}))(MuiDialogActions);
-
-export default function CustomizedDialogs() {
-  const [open, setOpen] = React.useState(false);
-
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
   const handleClose = () => {
-    setOpen(false);
+    props.setHitModal(false);
   };
 
   return (
-    <div className={modalStyles.modal}>
-      <Button variant="outlined" color="primary" onClick={handleClickOpen}>
-        关于我们
-      </Button>
-      <Dialog onClose={handleClose} aria-labelledby="customized-dialog-title" open={open}>
-        <DialogTitle id="customized-dialog-title" onClose={handleClose}>
-          关于我们
-        </DialogTitle>
-        <DialogContent dividers>
-          <Typography gutterBottom>
-            北京中医药大学管理学院学生，想做些对世界有意义的事情
-          </Typography>
-        </DialogContent>
-        <DialogActions>
-          <Button autoFocus onClick={handleClose} color="primary">
-            关闭
-          </Button>
-        </DialogActions>
-      </Dialog>
+    <div className="fixed z-10 inset-0 overflow-y-auto" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+      <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+        {/*
+        Background overlay, show/hide based on modal state.
+
+        Entering: "ease-out duration-300"
+        From: "opacity-0"
+        To: "opacity-100"
+        Leaving: "ease-in duration-200"
+        From: "opacity-100"
+        To: "opacity-0"
+        */}
+        <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" aria-hidden="true"></div>
+
+        {/* This element is to trick the browser into centering the modal contents. */}
+        <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
+
+        {/*
+        Modal panel, show/hide based on modal state.
+
+        Entering: "ease-out duration-300"
+        From: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+        To: "opacity-100 translate-y-0 sm:scale-100"
+        Leaving: "ease-in duration-200"
+        From: "opacity-100 translate-y-0 sm:scale-100"
+        To: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+    */}
+        <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+          <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+            <div className="sm:flex sm:items-start">
+              <div className="mx-auto flex-shrink-0 flex items-center justify-center sm:mx-0 sm:h-10 sm:w-10">
+                {/* Heroicon name: outline/exclamation */}
+                <InfoIcon fontSize="large"/>
+              </div>
+              <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                <h3 className="text-xl leading-6 font-medium text-gray-900" id="modal-title">
+                  关于我们
+                </h3>
+                <div className="mt-2">
+                  <p className="text-gray-500">
+                    北京中医药大学管理学院学生，想做些对世界有意义的事
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+
+            <button type="button" onClick={handleClose} className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">
+              关闭
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
 import React, { useState } from "react";
 import SearchIcon from "@material-ui/icons/Search";
 import CloseRoundedIcon from "@material-ui/icons/CloseRounded";
-import Button from "@material-ui/core/Button";
 
 import { useRouter } from "next/router";
 import Modal from "../components/Modal";
 
 function Home() {
   const [inputValue, setInputValue] = useState("");
+  const [hitModal, setHitModal] = useState(false);
 
   const router = useRouter();
 
@@ -38,30 +38,39 @@ function Home() {
     setInputValue(e.target.value);
   };
 
-
+  const handleAboutUS = () => {
+    if (hitModal === true)
+      setHitModal(false);
+    else
+      setHitModal(true);
+  };
 
   const handleClearContent = () => {
     setInputValue("");
   };
 
   const home = (
+
     <div className="min-w-max h-screen flex flex-col">
-      <div className="mt-56 flex-auto">
+      <div className="mt-64 flex-auto">
         <h1 className="text-5xl text-center">中医药搜索引擎</h1>
         <form className="" onSubmit={handleSubmit}>
-          <div className="flex items-center mt-28 border border-gray-300 rounded-lg h-12 py-4 px-4 mt-15 mx-auto w-3/4 max-w-xl">
+          <div className="flex items-center mt-24 border border-gray-300 rounded-lg h-12 py-4 px-4 mt-15 mx-auto w-3/4 max-w-xl">
             <SearchIcon className="fill-current text-gray-400 hover:text-black" onClick={handleSearch} />
             <input className="h-8 flex-1 py-2 px-4 text-xl focus:outline-none"
               value={inputValue} onChange={handleInputChange} />
             <CloseRoundedIcon className="ill-current text-gray-400 hover:text-black" onClick={handleClearContent} />
           </div>
           <div className="flex justify-between max-w-xs min-w-max mx-auto mt-16 ">
-            <Button className="w-20" variant="outlined" type="submit">搜索</Button>
-            <Modal className="w-20 ml-36" />
+            <button className="h-12 px-4 w-32 rounded-lg  tracking-widest font-extrabold border-2 border-gray-600  active:border-0 focus:ring-2 focus:ring-black focus:ring-opacity-50" type="submit">搜索</button>
+            <button className="h-12 px-4 w-32 rounded-lg text-gray-50 tracking-widest font-extrabold bg-gray-400 hover:bg-gray-500 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-opacity-50" type="button" onClick={handleAboutUS}>关于我们</button>
+
+            {hitModal ? <Modal className="w-20 ml-36" hitModal={hitModal} setHitModal={setHitModal} /> : null}
           </div>
         </form>
       </div>
     </div>
+
   );
   return (home);
 }
